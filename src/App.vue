3fd91cbcd385792691f086a97b15d88d1f311c75
Agni Rakai Sahakarya <rakai.sahakarya@gmail.com>
2022-12-27 23:25:58 +0700

    feat : add quasar

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header class="bg-white text-black" height-hint="98">
+      <q-toolbar>
+        <q-toolbar-title class="text-center">
+          <q-img src="./assets/logo-light.png" spinner-color="white" style="max-width: 300px; margin: 10px;" />
+          <h6 class="text-weight-light" style="margin: 0px;">Small 3D & Gamedev Team</h6>
+        </q-toolbar-title>
+      </q-toolbar>
+
+      <q-tabs align="center">
+        <q-route-tab to="/" label="Home" />
+        <q-route-tab to="/page3" label="Our Project" />
+      </q-tabs>
+    </q-header>
+
+    <q-page-container>
+      <HomePage />
+    </q-page-container>
+
+  </q-layout>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+import HomePage from './components/Home.vue'
 
 export default {
-  name: 'App',
+  name: 'LayoutDefault',
+
   components: {
-    HelloWorld
+    HomePage
+  },
+
+  setup() {
+    return {
+      leftDrawerOpen: ref(false)
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { Quasar } from 'quasar'
+import quasarUserOptions from './quasar-user-options'
+import MasonryWall from '@yeger/vue-masonry-wall'
 
-createApp(App).mount('#app')
+createApp(App).use(Quasar, quasarUserOptions).use(MasonryWall).mount('#app')
